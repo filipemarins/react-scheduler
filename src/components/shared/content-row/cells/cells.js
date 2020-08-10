77bda44c1e08620e12f57ba9f1a3c593fa8faf7d
filipemarins <lipe_bm@yahoo.com.br>
2020-08-10 20:03:43 -0300

    Refactor cells to function component

--- src/components/shared/content-row/cells/cells.js
+++ src/components/shared/content-row/cells/cells.js
@@ -1,49 +1,37 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useContext } from 'react';
+import { arrayOf, instanceOf } from 'prop-types';
 import clsx from 'clsx';
 
+import { SchedulerContext } from 'utils/scheduler-context';
 import * as dates from 'utils/dates';
 
-class Cells extends React.Component {
-  render() {
-    const {
-      range,
-      currentDate,
-      components: { dateCellWrapper: Wrapper },
-    } = this.props;
+const Cells = ({ range }) => {
+  const {
+    currentDate,
+    components: { dateCellWrapper: Wrapper },
+  } = useContext(SchedulerContext);
 
-    return (
-      <div className="rbc-row-bg">
-        {range.map((date, index) => {
-          return (
-            <Wrapper key={index} value={date} range={range}>
-              <div
-                className={clsx(
-                  'rbc-day-bg',
-                  dates.eq(date, currentDate, 'day') && 'rbc-today',
-                  currentDate &&
-                    dates.month(currentDate) !== dates.month(date) &&
-                    'rbc-off-range-bg'
-                )}
-              />
-            </Wrapper>
-          );
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="rbc-row-bg">
+      {range.map((date) => {
+        return (
+          <Wrapper key={date} value={date} range={range}>
+            <div
+              className={clsx(
+                'rbc-day-bg',
+                dates.eq(date, currentDate, 'day') && 'rbc-today',
+                currentDate && dates.month(currentDate) !== dates.month(date) && 'rbc-off-range-bg'
+              )}
+            />
+          </Wrapper>
+        );
+      })}
+    </div>
+  );
+};
 
 Cells.propTypes = {
-  currentDate: PropTypes.instanceOf(Date).isRequired,
-
-  components: PropTypes.object.isRequired,
-
-  container: PropTypes.func,
-
-  range: PropTypes.arrayOf(PropTypes.instanceOf(Date)),
-  rtl: PropTypes.bool,
-  type: PropTypes.string,
+  range: arrayOf(instanceOf(Date)).isRequired,
 };
 
 export default Cells;
